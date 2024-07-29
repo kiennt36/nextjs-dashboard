e5f881a0f35e1c05ce06efc7415ee57c35ed3928
kiennt-phenikaa-x <kiennt2@phenikaa-x.com>
2024-07-30 02:15:31 +0700

    fix lỗi path '@/...'

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import {Inter, Lusitana} from "next/font/google" // import google font
+
+export const inter = Inter({subsets: ["latin"]}) // export Inter font with subsets is latin
+export const lusitana = Lusitana({subsets: ["latin"], weight: ["400", "700"]}) // export Lusitana font with subsets is latin and weight is 400, 700
